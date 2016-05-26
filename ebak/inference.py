f46315adbf64e467a447db5af8ad7eaf48763c28
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-05-26 12:06:51 -0400

    added dumb-ass lnlikelihood function

--- ebak/inference.py
+++ ebak/inference.py
@@ -7,8 +7,8 @@ import astropy.units as u
 from astropy.constants import c
 import numpy as np
 
+c_kms = c.to(u.km/u.s).value
 
-@u.quantity_input(v1=u.km/u.s, v2=u.km/u.s)
 def get_design_matrix(data_spec, ref_spec, v1, v2):
     """
     Input are two spectra as dictionaries: the data spectrum must have
@@ -22,8 +22,10 @@ def get_design_matrix(data_spec, ref_spec, v1, v2):
     ----------
     data_spec : dict
     ref_spec : dict
-    v1 : astropy.units.Quantity
-    v2 : astropy.units.Quantity
+    v1 : float
+        In km/s
+    v2 : float
+        In km/s
 
     Returns
     -------
@@ -31,14 +33,22 @@ def get_design_matrix(data_spec, ref_spec, v1, v2):
         Design matrix.
     """
     X = np.ones((3, data_spec['wvln'].shape[0]))
-    X[1] = ref_spec['interp'](data_spec['wvln'] * (1 + v1/c)) # this is only good to first order in (v/c)
-    X[2] = ref_spec['interp'](data_spec['wvln'] * (1 + v2/c))
+    X[1] = ref_spec['interp'](data_spec['wvln'] * (1 + v1/c_kms)) # this is only good to first order in (v/c)
+    X[2] = ref_spec['interp'](data_spec['wvln'] * (1 + v2/c_kms))
+
+    # HACK: where there are extrpolation errors, set to zero and make error infinite
+    any_nan = np.isnan(X).any(axis=0)
+    if any_nan.any():
+        X[:,any_nan] = 0.
+        ref_spec['flux_err'] = np.inf
+
     return X
 
-@u.quantity_input(v1=u.km/u.s, v2=u.km/u.s)
-def get_optimal_chisq(data_spec, ref_spec, v1, v2):
+def get_optimal_chisq_pars(data_spec, ref_spec, v1, v2):
     X = get_design_matrix(data_spec, ref_spec, v1, v2)
-    return np.linalg.solve(X.dot(X.T), X.dot(data_spec['flux']))
+    C_inv_diag = 1 / (data_spec['flux_err']**2 + ref_spec['flux_err']**2) + 1e-15 # HACK: magic number to prevent singular matrix
+    return np.linalg.solve((X * C_inv_diag[None]).dot(X.T),
+                           (X * C_inv_diag[None]).dot(data_spec['flux']))
 
 def make_synthetic_spectrum(X, pars):
     """
@@ -48,3 +58,34 @@ def make_synthetic_spectrum(X, pars):
     """
     return X.T.dot(pars)
 
+def ln_likelihood(v1_v2, data_spec, ref_spec):
+    """
+    Input are two spectra as dictionaries: the data spectrum must have
+    'wvln' and 'flux' keys and the reference spectrum must have an 'interp'
+    key that contains a interpolating function that evaluates the reference
+    spectrum on a given wavelength grid.
+
+    Parameters
+    ----------
+    v1_v2 : iterable
+        The two velocities, v1, v2
+    data_spec : dict
+    ref_spec : dict
+
+    Returns
+    -------
+    lnlikelihood : float
+
+    """
+    v1,v2 = v1_v2
+    X = get_design_matrix(data_spec, ref_spec, v1, v2)
+    inv_var = 1 / (data_spec['flux_err']**2 + ref_spec['flux_err']**2)
+    C_inv_diag = inv_var + 1e-15 # HACK: magic number to prevent singular matrix
+
+    X_Cinv_XT = (X * C_inv_diag[None]).dot(X.T)
+    optimal_pars = np.linalg.solve(X_Cinv_XT,
+                                   (X * C_inv_diag[None]).dot(data_spec['flux']))
+
+    model_spec = make_synthetic_spectrum(X, optimal_pars)
+    _,logdet = np.linalg.slogdet(X_Cinv_XT)
+    return -0.5 * np.sum((data_spec['flux'] - model_spec)**2 * inv_var) - 0.5*logdet

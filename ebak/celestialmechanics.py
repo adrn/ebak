07322597b4ed9c3a9ba86f9a8b2c817a307a3cc7
davidwhogg <david.hogg@nyu.edu>
2016-05-27 16:43:20 -0400

    testing

--- ebak/celestialmechanics.py
+++ ebak/celestialmechanics.py
@@ -25,7 +25,7 @@ def mean_anomaly_from_eccentric_anomaly(E, e):
     """
     return E - e * np.sin(E)
 
-def eccentric_anomaly_from_mean_anomaly(M, e, tol=1e-8, maxiter=100):
+def eccentric_anomaly_from_mean_anomaly(M, e, tol=1e-14, maxiter=100):
     """
     # inputs:
     - M: mean anomaly (rad)
@@ -153,23 +153,42 @@ def rv_from_elements(P, a, sini, e, omega, time, time0):
     rv = r * np.cos(omega + f) * sini * dfdt + np.sin(omega + f) * sini * drdt
     return rv
 
-if __name__ == "__main__":
-    import pylab as plt
+def test_everything():
     np.random.seed(42)
-    t0 = 0. # d
-    t1 = 400. # d
-    dt = (1. / 24.) # d
-    times = np.arange(t0 + 0.5 * dt, t1)
-    for i in range(32):
-        suffix = "{:03d}".format(i)
+    tt0 = 0. # d
+    tt1 = 400. # d
+    for n in range(256):
         P = np.exp(np.log(10.) + np.log(400./10.) * np.random.uniform()) # d
-        a = (P / 300.) ** (2. / 3.) # made up shit AU
-        e = 0.5 # testing
+        a = (P / 300.) ** (2. / 3.) # AU
+        e = np.random.uniform()
         omega = 2. * np.pi * np.random.uniform() # rad
-        time0 = t0 + (t1 - t0) * np.random.uniform()
-        rvs = np.array([rv_from_elements(P, a, 1.0, e, omega, t, time0) for t in times])
-        plt.clf()
-        plt.plot(times, rvs, "k-")
-        pfn = "rvtest_" + suffix + ".png"
-        print(pfn)
-        plt.savefig(pfn)
+        time0, time = tt0 + (tt1 - tt0) * np.random.uniform(size=2) # d
+        sini = 1.0
+        print("testing", P, a, sini, e, omega, time, time0)
+        big = 65536.0 # MAGIC
+        dt = P / big # d ; MAGIC
+        dMdt = 2. * np.pi / P # rad / d
+        threetimes = [time, time - 0.5 * dt, time + 0.5 * dt]
+        M, M1, M2 = ((t - time0) * dMdt for t in threetimes)
+        E, E1, E2 = (eccentric_anomaly_from_mean_anomaly(MM, e) for MM in [M, M1, M2])
+        dEdM = d_eccentric_anomaly_d_mean_anomaly(E, e)
+        dEdM2 = (E2 - E1) / (M2 - M1)
+        if np.abs(dEdM - dEdM2) > (1. / big):
+            print("dEdM", dEdM, dEdM2, dEdM - dEdM2)
+            assert False
+        f, f1, f2 = (true_anomaly_from_eccentric_anomaly(EE, e) for EE in [E, E1, E2])
+        dfdE = d_true_anomaly_d_eccentric_anomaly(E, f, e)
+        dfdE2 = (f2 - f1) / (E2 - E1)
+        if np.abs(dfdE - dfdE2) > (1. / big):
+            print("dfdE", dfdE, dfdE2, dfdE - dfdE2)
+            assert False
+        Z, Z1, Z2 = (Z_from_elements(P, a, sini, e, omega, tt, time0) for tt in threetimes)
+        rv = rv_from_elements(P, a, sini, e, omega, time, time0)
+        rv2 = (Z2 - Z1) / dt
+        if np.abs(rv - rv2) > (a / P) * (1. / big):
+            print("RV", rv, rv2, rv - rv2)
+            assert False
+    return True
+
+if __name__ == "__main__":
+    test_everything()

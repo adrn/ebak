68160e1563a0e4172917468c663a37564e6dc0c1
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-08-09 08:17:25 +0200

    move params to model class

--- ebak/celestialmechanics_class.py
+++ ebak/celestialmechanics_class.py
@@ -24,7 +24,7 @@ __all__ = ['RVOrbit', 'RVData', 'SimulatedRVOrbit', 'OrbitModel']
 usys = UnitSystem(u.au, u.day, u.radian, u.Msun)
 _ivar_disk = (1 / (30.*u.km/u.s)**2).decompose(usys).value # used in ln_prior below
 _G = G.decompose(usys).value
-lnG_4pi2 = np.log(_G/(4*np.pi**2))
+jitter_scale = (0.3*u.km/u.s).decompose(usys).value # HACK: magic number, typical vel err.
 
 class RVOrbit(object):
     """
@@ -81,30 +81,6 @@ class RVOrbit(object):
     @property
     def units(self):
         return usys
-
-    @classmethod
-    def from_vec(cls, p):
-        (ln_P, ln_a_sin_i, sqrte_cos_pomega,
-         sqrte_sin_pomega, _t0, _v0) = p
-        ecc = sqrte_cos_pomega**2 + sqrte_sin_pomega**2
-        _omega = np.arctan2(sqrte_sin_pomega, sqrte_cos_pomega)
-        return cls(P=np.exp(ln_P)*usys['time'], a_sin_i=np.exp(ln_a_sin_i)*usys['length'],
-                   ecc=ecc, omega=_omega*usys['angle'], t0=_t0,
-                   v0=_v0*usys['length']/usys['time'])
-
-    def set_par_from_vec(self, p):
-        (ln_P, ln_a_sin_i, sqrte_cos_pomega,
-         sqrte_sin_pomega, self._t0, self._v0) = p
-        self.ecc = sqrte_cos_pomega**2 + sqrte_sin_pomega**2
-        self._omega = np.arctan2(sqrte_sin_pomega, sqrte_cos_pomega)
-        self._P = np.exp(ln_P)
-        self._a_sin_i = np.exp(ln_a_sin_i)
-
-    def get_par_vec(self):
-        return np.array([np.log(self._P), np.log(self._a_sin_i),
-                         np.sqrt(self.ecc)*np.cos(self._omega),
-                         np.sqrt(self.ecc)*np.sin(self._omega),
-                         self._t0, self._v0])
 
 class SimulatedRVOrbit(RVOrbit):
 
@@ -236,33 +212,48 @@ class OrbitModel(object):
     data : RVData
 
     """
-    def __init__(self, orbit, data):
-        self.orbit = orbit
+    def __init__(self, data, orbit=None, s=0.*u.km/u.s):
         self.data = data
+
+        if orbit is None:
+            orbit = SimulatedRVOrbit(np.nan*u.yr, np.nan*u.au, np.nan,
+                                     np.nan*u.radian, np.nan, v0=np.nan*u.km/u.s)
+        self.orbit = orbit
+
+        self._s = s.decompose(usys).value
+
+    @property
+    def s(self):
+        return self._s*usys['length']/usys['time']
 
     def ln_likelihood(self):
         rvs = self.orbit._generate_rv_curve(self.data._t)
-        return -0.5 * self.data._ivar * (self.data._rv - rvs)**2
+        new_ivar = self.data._ivar / (1 + self._s**2 * self.data._ivar)
+        chi2 = new_ivar * (self.data._rv - rvs)**2
+
+        return -0.5*chi2 + 0.5*np.log(new_ivar)
 
     def ln_prior(self):
         lnp = 0.
 
-        # prior on mass function
-        K = 2*np.pi / (self.orbit.P * np.sqrt(1-self.orbit.ecc**2)) * self.orbit.a_sin_i
-        m_f = self.orbit.P * K**3 / (2*np.pi*G) * (1-self.orbit.ecc**2)**(3/2.)
+        # Mass function: log-normal centered on ln(3)
+        K = 2*np.pi / (self.orbit._P * np.sqrt(1-self.orbit.ecc**2)) * self.orbit._a_sin_i
+        m_f = self.orbit._P * K**3 / (2*np.pi*_G) * (1-self.orbit.ecc**2)**(3/2.)
         lnp += -0.5 * (np.log(m_f) - np.log(3.))**2 / (2.)**2
         if m_f < 0:
             return -np.inf
 
-        # assumes sampler is stepping in log(P)
-        if self.orbit._P < 1. or self.orbit._P > 8192*365.: # days
+        # Orbital period: assumes sampler is stepping in log(P)
+        if self.orbit._P < 0.1 or self.orbit._P > 8192*365.: # days
             return -np.inf
 
+        # Semi-major axis and inclination
         if 1E-6 < self.orbit._a_sin_i < 16384.: # au
             lnp += -np.log(self.orbit._a_sin_i)
         else:
             return -np.inf
 
+        # Eccentricity
         if self.orbit.ecc < 0. or self.orbit.ecc > 1.:
             return -np.inf
 
@@ -272,8 +263,13 @@ class OrbitModel(object):
             self.orbit._t0 > (_t0_epoch+1.5*self.orbit._P)):
             return -np.inf
 
-        # Gaussian with velocity dispersion of the disk
+        # Systemic velocity: Gaussian with velocity dispersion of the disk
         lnp += -0.5 * _ivar_disk * self.orbit._v0**2
+
+        # Jitter: for velocity error model
+        if self._s < 0.:
+            return -np.inf
+        lnp += -self._s / jitter_scale
 
         return lnp
 
@@ -284,8 +280,46 @@ class OrbitModel(object):
         return lnp + self.ln_likelihood().sum()
 
     def __call__(self, p):
-        self.orbit.set_par_from_vec(p)
+        self.set_par_from_vec(p)
         return self.ln_posterior()
+
+    def from_vec(self, p):
+        (ln_P, ln_a_sin_i, sqrte_cos_pomega,
+         sqrte_sin_pomega, _t0, _v0, _s) = p
+
+        ecc = sqrte_cos_pomega**2 + sqrte_sin_pomega**2
+        _omega = np.arctan2(sqrte_sin_pomega, sqrte_cos_pomega)
+
+        orbit = SimulatedRVOrbit(P=np.exp(ln_P)*usys['time'],
+                                 a_sin_i=np.exp(ln_a_sin_i)*usys['length'],
+                                 ecc=ecc, omega=_omega*usys['angle'], t0=_t0,
+                                 v0=_v0*usys['length']/usys['time'])
+
+        # nuisance parameters
+        s = _s*usys['length']/usys['time']
+
+        return self.__class__(data=self.data, orbit=orbit, s=s)
+
+    def set_par_from_vec(self, p):
+        (ln_P, ln_a_sin_i, sqrte_cos_pomega,
+         sqrte_sin_pomega, _t0, _v0, _s) = p
+
+        self.orbit.ecc = sqrte_cos_pomega**2 + sqrte_sin_pomega**2
+        self.orbit._omega = np.arctan2(sqrte_sin_pomega, sqrte_cos_pomega)
+        self.orbit._P = np.exp(ln_P)
+        self.orbit._a_sin_i = np.exp(ln_a_sin_i)
+        self.orbit._t0 = _t0
+        self.orbit._v0 = _v0
+
+        # nuisance parameters
+        self._s = _s
+
+    def get_par_vec(self):
+        return np.array([np.log(self.orbit._P), np.log(self.orbit._a_sin_i),
+                         np.sqrt(self.orbit.ecc)*np.cos(self.orbit._omega),
+                         np.sqrt(self.orbit.ecc)*np.sin(self.orbit._omega),
+                         self.orbit._t0, self.orbit._v0,
+                         self._s])
 
     def vec_to_plot_pars(self, p):
         """
@@ -296,7 +330,8 @@ class OrbitModel(object):
         if p.ndim > 2:
             raise ValueError("Shape of input p must be <= 2 (e.g., pass flatchain")
 
-        orbit = SimulatedRVOrbit.from_vec(p)
+        model = self.from_vec(p)
+        orbit = model.orbit
 
         # TODO: is K correct here?
         K = 2*np.pi / (orbit.P * np.sqrt(1-orbit.ecc**2)) * orbit.a_sin_i
@@ -304,4 +339,5 @@ class OrbitModel(object):
 
         return np.vstack((np.log(orbit.P.to(u.day).value), m_f.to(u.Msun).value,
                           orbit.ecc, orbit.omega.to(u.degree).value,
-                          orbit.t0.mjd, orbit.v0.to(u.km/u.s).value))
+                          orbit.t0.mjd, orbit.v0.to(u.km/u.s).value,
+                          model.s.to(u.km/u.s).value))

15106949f25ebada663664d109083ed38adbeb9d
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-08-09 12:03:25 +0200

    adding a functional test and making sure plot pars works for chain links

--- ebak/singleline/test_inference.py
+++ ebak/singleline/test_inference.py
@@ -8,6 +8,8 @@ import astropy.units as u
 import matplotlib.pyplot as plt
 import numpy as np
 import pytest
+import emcee
+import corner
 
 from ..celestialmechanics_class import SimulatedRVOrbit
 from .data import RVData
@@ -45,5 +47,52 @@ def test_orbitmodel():
             OrbitModel(data=data, orbit=orbit).ln_posterior())
 
     model = OrbitModel(data=data, orbit=orbit)
-    print(model.get_par_vec())
+    p0 = model.get_par_vec()
 
+    plot_pars = model.vec_to_plot_pars(p0)
+    many_p0 = np.repeat(p0[None], 16, axis=0)
+    many_plot_pars = model.vec_to_plot_pars(many_p0)
+
+    for i in range(many_plot_pars.shape[0]):
+        assert np.allclose(many_plot_pars, plot_pars)
+
+# TODO: add decorator for slow test
+def test_sample_from_prior():
+    """
+    Functional test: run with no data to sample from prior
+    """
+    n_walkers = 128
+    n_steps = 2048
+
+    no_data = RVData(np.array([]),
+                     rv=[]*u.km/u.s,
+                     ivar=[]/(u.km/u.s)**2)
+
+    init_orbit = SimulatedRVOrbit(P=521.*u.day, a_sin_i=2*u.au, ecc=0.3523,
+                                  omega=21.85*u.degree, phi0=-11.723*u.degree,
+                                  v0=27.41*u.km/u.s)
+
+    model = OrbitModel(data=no_data, orbit=init_orbit)
+    assert np.isfinite(model.ln_posterior())
+
+    p0 = emcee.utils.sample_ball(model.get_par_vec(),
+                                 1E-6*model.get_par_vec() + 1E-6,
+                                 size=n_walkers)
+    sampler = emcee.EnsembleSampler(n_walkers, dim=p0.shape[1],
+                                    lnpostfn=model)
+
+    pos,_,_ = sampler.run_mcmc(p0, N=n_steps)
+
+    # walker plots
+    fig,axes = plt.subplots(p0.shape[1], 1, figsize=(5,18), sharex=True)
+    for i in range(p0.shape[1]):
+        axes[i].set_ylabel(model.vec_labels[i])
+        axes[i].plot(sampler.chain[...,i].T, drawstyle='steps', alpha=0.1, marker=None)
+    fig.tight_layout()
+
+    # corner plot
+    flatchain = np.vstack(sampler.chain[:,-256:])
+    plot_pars = model.vec_to_plot_pars(flatchain.T).T
+    fig = corner.corner(plot_pars, labels=model.plot_labels)
+
+    plt.show()

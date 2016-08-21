b4127a18837c9ed80167793d33c87ec2e528018c
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-08-20 22:39:02 -0500

    troup slurm

---
+++ slurm/submit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -J troup           # job name
+#SBATCH -o troup.o%j             # output and error file name (%j expands to jobID)
+#SBATCH -n 128                   # total number of mpi tasks requested
+#SBATCH -p normal          # queue (partition) -- normal, development, etc.
+#SBATCH -A TG-AST150023         # Project ID
+#SBATCH -t 00:10:00             # run time (hh:mm:ss) - 1.5 hours
+#SBATCH --mail-user=amp2217@columbia.edu
+#SBATCH --mail-type=begin       # email me when the job starts
+#SBATCH --mail-type=end         # email me when the job finishes
+
+export NBURN=256
+export NSTEPS=4096
+export NWALKERS=512
+export SAMPLER='kombine'
+
+cd $WORK/projects/ebak/scripts/
+
+source activate ebak
+
+ibrun python mcmc-troup.py -v -s $SAMPLER -o --n-steps=$NSTEPS --n-burnin=$NBURN --n-walkers=$NWALKERS --index=$INDEX --mpi
